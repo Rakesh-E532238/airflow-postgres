9ea6beae2894b04590fb0f932dfcfbb9446f09e7
Rakesh Venuturumilli <Rakesh.Venuturumilli@Honeywell.com>
2020-07-06 16:45:04 -0700

    added permissions in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
-if [ "$1" = "webserver" ]
-then
-    exec airflow webserver
-fi
-​
-if [ "$1" = "scheduler" ]
-then
-    exec airflow scheduler
-fi
+sudo chown -R ubuntu /usr/local/airflow
+sudo chgrp -R ubuntu /usr/local/airflow
+# if [ "$1" = "webserver" ]
+# then
+#     exec airflow webserver
+# fi
+# ​
+# if [ "$1" = "scheduler" ]
+# then
+#     exec airflow scheduler
+# fi
